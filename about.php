6a6515ce7a182282eca855225a3671217858b802
Michael Rose <rosejmichael71@gmail.com>
2020-01-30 19:38:35 -0700

    This is the initial commit

---
+++ about.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="main.css">
+    <title>My Blog</title>
+</head>
+<body>
+<div id = "nav">
+		<div class="title">
+			<h1>About Michael Rose</h1>
+		</div>
+		<div class="menu"> 	
+				<ul>
+					<li><a 	href = "index.php"> Home </a></li>
+					<li><a 	href = "about.php"><span class = "current"> About </span></a></li>
+				</ul>
+		</div>
+</div>
+		<div id = "mainbody">
+				<h2>About Michael Rose</h2>
+				<h3>This is a site that is in progress....</h3>
+				<p>In another life, I was working at Packard Bell/NEC customer service.  I had gotten tired of the corporate life, and wanted a change.
+					That is when I decided to change my career.</p>
+				<p>I'm a truck driver by trade.  In 2019 I had decided that it was time for me to own my own business.  For 10 months I was 
+				purchasing my own truck and leasing back to KMB Brothers.  The engine blew and I couldn't afford to fix it.</p>
+				<p>After that catastrophe, I had become unemployed for a couple of months.  I had attempted to get a loan to either
+					fix my truck or get another one.  However, I didn't have any savings and didn't have a down payment.</p>
+				<p>While I was attempting to save my business, I had continued looking for work.  I was doing some temporary work
+					and even washed some dishes.  Eventually, I had found Sunrise Express, a trucking company out of Nebraska.  They would
+					allow me to keep my newly aquired puppy dog Nuvo.</p>
+				<p>Even though I was up and running again, I wanted to save my business.  I was doing research on Web Development and decided
+					that I would give it a try.</p>
+				<p>And that is my story.  Right now I'm working on this site and still learning.  I can bring a whole new perspective to the 
+					industry.  I have worked various manual labor jobs in the past.  With the experience that I have with computers, I'll bring 
+					a new perspective to the business.</p>
+				<h2>The story continues...</h2>
+		</div>
+	<div id="footer">
+		<h2>Copyright by &copy Rose Rising Enterprises LLC</h2>
+	</div>
+
+</body>
+</html>
